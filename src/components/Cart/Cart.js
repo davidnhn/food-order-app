06722b4ec34b65e5bb4f77e13031022518cfde50
David DN. Nahon <dnahon@ads31.com>
2022-09-12 17:01:44 +0200

    erreur tofixed on undefined

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,21 +1,29 @@
+import { useContext } from 'react';
+import CartContext from '../../store/cart-context';
 import Modal from '../UI/Modal';
 import classes from './Cart.module.css';
 const Cart = (props) => {
-  const cartItems = [{ id: 'c1', name: 'suchi', amount: 2, price: 12.99 }].map(
-    (item) => <li>{item.name}</li>
-  );
+  const cartCtx = useContext(CartContext);
+  const cartItems = cartCtx.items.map((item) => <li>{item.name}</li>);
+
+  const totalAmount = `$${cartCtx.totalAmout.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
   return (
     <Modal onClose={props.onClose}>
       <ul className={classes['cart-items']}>{cartItems}</ul>
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>35.62</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
         <button className={classes['button--alt']} onClick={props.onClose}>
           Close
         </button>
-        <button className={classes.button}>Order</button>
+        {hasItems && (
+          <button button className={classes.button}>
+            Order
+          </button>
+        )}
       </div>
     </Modal>
   );
