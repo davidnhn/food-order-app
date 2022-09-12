6960f5fee9c923945067189d6fc513387176808f
David DN. Nahon <dnahon@ads31.com>
2022-09-12 12:16:33 +0200

    ajout modal

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React, { Fragment } from 'react';
+import React, { useState } from 'react';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider';
 function App() {
+  const [cartShown, setCartShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartShown(true);
+  };
+  const hideCartHandler = () => {
+    setCartShown(false);
+  };
   return (
-    <Fragment>
-      <Cart />
-      <Header />
+    <CartProvider>
+      {cartShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -5,14 +5,16 @@ const Cart = (props) => {
     (item) => <li>{item.name}</li>
   );
   return (
-    <Modal>
-      <ul className={classes['card-items']}>{cartItems}</ul>
+    <Modal onClose={props.onClose}>
+      <ul className={classes['cart-items']}>{cartItems}</ul>
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>35.62</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes['button--alt']}>Close</button>
+        <button className={classes['button--alt']} onClick={props.onClose}>
+          Close
+        </button>
         <button className={classes.button}>Order</button>
       </div>
     </Modal>
