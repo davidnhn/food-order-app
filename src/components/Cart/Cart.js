75954733e275739fcb99088cd9bd58ad2fcef43b
David DN. Nahon <dnahon@ads31.com>
2022-09-13 12:13:08 +0200

    add remove item

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -5,24 +5,29 @@ import classes from './Cart.module.css';
 import CartItem from './CartItem';
 const Cart = (props) => {
   const cartCtx = useContext(CartContext);
-  const cartItemRemoveHandler = (id) => {};
-  const cartItemAddHandler = (item) => {};
-  const cartItems = cartCtx.items.map((item) => (
-    <CartItem
-      key={item.id}
-      name={item.name}
-      amount={item.amount}
-      price={item.price}
-      onRemove={cartItemRemoveHandler.bind(null, item.id)}
-      onAdd={cartItemAddHandler.bind(null, item)}
-    ></CartItem>
-  ));
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({ ...item, amount: 1 });
+  };
 
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
   return (
     <Modal onClose={props.onClose}>
-      <ul className={classes['cart-items']}>{cartItems}</ul>
+      <ul className={classes['cart-items']}>
+        {cartCtx.items.map((item) => (
+          <CartItem
+            key={item.id}
+            name={item.name}
+            amount={item.amount}
+            price={item.price}
+            onRemove={cartItemRemoveHandler.bind(null, item.id)}
+            onAdd={cartItemAddHandler.bind(null, item)}
+          ></CartItem>
+        ))}
+      </ul>
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>{totalAmount}</span>
